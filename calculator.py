cc3bf002aeb5ea25d7f8ee8fb558d5802f06798f
Ozodbek-05 <ozodbekhoshimov53@gmail.com>
2025-07-04 09:45:26 +0500

    Add calculator.py file

---
+++ calculator.py
@@ -0,0 +1,9 @@
+a = int(input("a = "))
+b = int(input("b = "))
+print("Yig'indi:", a + b)
+print("Ayirma:", a - b)
+print("Ko'paytma:", a * b)
+if b != 0:
+    print("Bo'linma:", a / b)
+else:
+    print("Bo'linganda maxraj 0 bo'lmasin!!!")
